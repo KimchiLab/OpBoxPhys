e596560caa56baeb53bbdbe67dd3d80d21b5edc3
ekimchi <ekimchi@users.noreply.github.com>
2020-05-22 09:32:13 -0400

    Refresh

--- CellStrToNums.m
+++
@@ -1,9 +0,0 @@
-function nums = CellStrToNums(cell_str)
-
-matches = regexp(cell_str, '\d+$', 'match');
-
-nums = nan(numel(cell_str),1);
-
-for i_cell = 1:numel(cell_str)
-    nums(i_cell) = str2double(matches{i_cell});
-end
