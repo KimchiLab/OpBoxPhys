e77b5c88e7af4190f3f1ed3f2763f06e6585aa34
EyalKimchiLab <137069418+EyalKimchiLab@users.noreply.github.com>
2024-08-01 10:47:34 -0500

    Added more boxes with text

--- OpBox Phys Scripts/OpBox_Stop.m
+++ OpBox Phys Scripts/OpBox_Stop.m
@@ -15,7 +15,7 @@ else
     for i_subj = 1:numel(subjects)
         subjects(i_subj) = subjects(i_subj).FileClose();
         
-        if ~isempty(subjects(i_subj).cam_id) && subjects(i_subj).cam_id > 0 && ~isempty(subjects(i_subj).cam)
+        if numel(subjects(i_subj).cam_id) && numel(subjects(i_subj).cam)
             try
                 stop(subjects(i_subj).cam);
                 close(get(subjects(i_subj).cam, 'DiskLogger'));
